b8d5bb951fc7e6ad2241ce152e809aebc70cf2bf
Allison Bessett <allison_17@hotmail.com>
2017-05-25 08:30:23 -0700

    Edit and delete buttons show on table

--- local-storage.js
+++ local-storage.js
@@ -2,17 +2,14 @@
 /*global $, jQuery, alert*/
 
 
-//TODO: Use localStorage to store data. 
-//TODO: Create JSON of array of objects
-//TODO: Read data from localStorage at start-up. 
 //TODO: Save to it whenever data changes. 
 //TODO: Add Edit/Delete button functionality
 
 
-//on add item click, show form, hide table
+
 
 $('document').ready(function () {
-  var bookTable = $('#bookTable'), bookForm = $('#bookForm'), e = event;
+  var bookTable = $('#bookTable'), bookForm = $('#bookForm'), e = event, arr = [], book = $('#book'), author = $('#author'), count = 0;
   
 //  bookForm.hide();
   
@@ -20,4 +17,28 @@ $('document').ready(function () {
     bookForm.show();
     bookTable.hide();
   });
+  function updateArray(book, author) {
+    arr.push({tBook: book, tAuthor: author});
+    console.log("working");
+  }
+  function updateTable() {
+    $('#bookTable tbody').html('<tr></tr>');
+    for (var i = 0; i < arr.length; i++) {
+    $('#bookTable tBody').append('<tr><td>' + arr[i].tBook + '</td>' + '<td>' + arr[i].tAuthor + '</td>' + '<td>' + "<button type = 'button' id='edit_button'>Edit</button>" + '</td>' + '<td>' + "<button type='button' id = 'delete_button'>Delete</button>" + '</td>' + '</tr>'); 
+    count++;
+    }
+    localStorage.setItem("arr", JSON.stringify(arr));
+    
+  }
+  
+  $('#submit').on('click', function (e) {
+    e.preventDefault();
+    updateArray(book.val(), author.val());
+    updateTable();
+    bookTable.show();
+    bookForm.hide();
+    
+    //call save settings function
+  });
+  
 });
